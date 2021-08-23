a1f3bb8444c881b52776aa83b39b845e704b35e4
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-08-23 20:32:45 +0100

    cleaned up

--- src/modules/status.js
+++
@@ -1,6 +0,0 @@
-const changeCompleteStatus = (objArrayList, taskObjIndex) => {
-  objArrayList[taskObjIndex - 1].completed = !objArrayList[taskObjIndex - 1].completed;
-  localStorage.setItem('TaskList', JSON.stringify(objArrayList));
-};
-
-export { changeCompleteStatus as default };
